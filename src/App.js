2f5f91f9d9f44bb5f9ad3ba63104cbe357868e9e
Vaibhav Rastogi - v0r00fr <vaibhav.rastogi0@walmart.com>
2020-03-28 19:23:32 -0400

    updating to react component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import image1 from './images.jpeg';
 
-function App() {
-  return (    
-    <div class="container">
-      <header class="header">
-        Route to Rescue
-      </header>
-      <div class="clearfix">
-        <div class="box" >
-        <img src={image1}/>
-        </div>
-        <div class="box" >
-        <ul class="a">
+import image1 from './images.jpeg';
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="container">
+        <header className="header">
+          Route to Rescue
+        </header>
+        <div className="clearfix">
+          <div className="box">
+          <picture>
+            <img src={image1} alt={"image loading"} />
+          </picture>
+          </div>
+          <div className="box">
+            <ul className="a">
               <li>Probability: 90%</li>
               <li>Number of Reportings: 5</li>
               <li>Emotion: FEAR 70%, SAD 83%</li>
-              <li>Location: Kenya, Africa -->DC, USA -->Atlanta, USA</li>
+              <li>Location: Kenya, Africa --&gt;DC, USA --&gt;Atlanta, USA</li>
               <li>Match in Missing Person Report: Yes</li>
               <li>Date Reported Missing: March 28, 2020</li>
             </ul>     
+          </div>
         </div>
-      </div>
-      <div class="clearfix">
-        <div class="box" >
-        <img src={image1}/>
-        </div>
-        <div class="box" >
-        <ul class="a">
+        <div className="clearfix">
+          <div className="box">
+            <picture>
+              <img src={image1} alt={"image loading"} />
+            </picture>
+          </div>
+          <div className="box">
+            <ul className="a">
               <li>Probability: 90%</li>
               <li>Emotion: FEAR 70%, DISGUSTED 43%</li>
               <li>Location: Atlanta, USA</li>
               <li>Match in Missing Person Report: Yes</li>
               <li>Date Reported Missing: March 28, 2020</li>
             </ul>     
+          </div>
         </div>
       </div>
-     </div>
-  );
-}
-
+    );
+  }
+}; 
 export default App;
