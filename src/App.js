3a31437836b4e8b192e72d554d0208466ce67a2c
Vaibhav Rastogi - v0r00fr <vaibhav.rastogi0@walmart.com>
2020-03-28 21:38:27 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 
 import image1 from './images.jpeg';
+import image2 from './image1.jpeg';
+import locationIcon from './location_icon.png'
 import React, { Component } from 'react';
 import './App.css';
-
+import { render } from "react-dom";
+import { Chart } from "react-google-charts";
 class App extends Component {
   render () {
     return (
@@ -10,39 +13,95 @@ class App extends Component {
         <header className="header">
           Route to Rescue
         </header>
-        <div className="clearfix">
-          <div className="box">
-          <picture>
-            <img src={image1} alt={"image loading"} />
-          </picture>
-          </div>
-          <div className="box">
-            <ul className="a">
-              <li>Probability: 90%</li>
-              <li>Number of Reportings: 5</li>
-              <li>Emotion: FEAR 70%, SAD 83%</li>
-              <li>Location: Kenya, Africa --&gt;DC, USA --&gt;Atlanta, USA</li>
-              <li>Match in Missing Person Report: Yes</li>
-              <li>Date Reported Missing: March 28, 2020</li>
-            </ul>     
-          </div>
-        </div>
+        <header className="header2">
+          Providing Rescue and helping lawmakers catch preprators
+        </header>
+        <hr className="line"/>
         <div className="clearfix">
           <div className="box">
             <picture>
               <img src={image1} alt={"image loading"} />
-            </picture>
+            </picture>                      
+          </div>
+          <div className="box">
+            <Chart
+              //width={'600px'}
+              height={'280px'}
+              chartType="BarChart"
+              loader={<div>Loading Chart</div>}
+              data={[
+                ['Emotion', '%'],
+                ['Happy', 10],
+                ['Sad', 60],
+                ['Fear', 89],
+                ['Shock',75],
+              ]}
+              options={{
+                // Material design options
+                title: "Emotion Analysis",
+                //backgroundColor:'#282c34'
+              }}
+              />
           </div>
           <div className="box">
             <ul className="a">
               <li>Probability: 90%</li>
-              <li>Emotion: FEAR 70%, DISGUSTED 43%</li>
-              <li>Location: Atlanta, USA</li>
+              <li>Face Match from other Reportings: 8</li>
+              <li>Last 5 Locations Reported:</li>
+              <ul className="a">
+                <li>Nairobi, Kenya</li>
+                <li>Mombasa, Kenya</li>
+                <li>Miami, USA</li>
+                <li>Vegas, USA</li>
+              </ul>  
               <li>Match in Missing Person Report: Yes</li>
-              <li>Date Reported Missing: March 28, 2020</li>
-            </ul>     
-          </div>
+              <li>Date Reported Missing: Mar 28, 2020</li>
+            </ul>  
+          </div>         
         </div>
+        <hr className="line"/>
+        <div className="clearfix">
+          <div className="box">
+            <picture>
+              <img src={image2} alt={"image loading"} />
+            </picture>
+          </div>
+          <div className="box">
+            <Chart
+              //width={'600px'}
+              height={'280px'}
+              chartType="BarChart"
+              loader={<div>Loading Chart</div>}
+              data={[
+                ['Emotion', '%'],
+                ['Happy', 7],
+                ['Sad', 90],
+                ['Fear', 40],
+                ['Shock',36],
+              ]}
+              options={{
+                // Material design options
+                title: "Emotion Analysis",
+                //backgroundColor:'#282c34'
+              }}
+              />
+          </div>
+          <div className="box">
+            <ul className="a">
+              <li>Probability: 79%</li>
+              <li>Face Match from other Reportings: 3</li>
+              <li>Last 5 Locations Reported:</li>
+              <ul className="a">
+                <li>Kolkata, India</li>
+                <li>San diego, USA</li>
+              </ul>  
+              <li>Match in Missing Person Report: Yes</li>
+              <li>Date Reported Missing: Feb 10, 2020</li>
+            </ul>  
+          </div>         
+        </div>
+        
+        
       </div>
     );
   }
