ed66ad3b5398b03e8a8ab2b8725c429bac3b61e9
Vaibhav Rastogi - v0r00fr <vaibhav.rastogi0@walmart.com>
2020-03-28 23:08:06 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/route-to-rescue/precache-manifest.136f31fc21f33b9ac7aff8449064a023.js"
+  "/route-to-rescue/precache-manifest.77c80aade3300e182092f63d4cac5279.js"
 );
 
 self.addEventListener('message', (event) => {
